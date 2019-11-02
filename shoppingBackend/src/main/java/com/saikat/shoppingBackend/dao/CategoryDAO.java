0bfe2a39c6acaed69c9a697996f89751b43b8eac
GhoshSaikat12 <Ghoshsaikat619@gmail.com>
2019-11-03 03:11:42 +0530

    incomplete backend integration

---
+++ shoppingBackend/src/main/java/com/saikat/shoppingBackend/dao/CategoryDAO.java
@@ -0,0 +1,11 @@
+package com.saikat.shoppingBackend.dao;
+
+import java.util.List;
+
+import com.saikat.shoppingBackend.dto.Category;
+
+public interface CategoryDAO {
+	
+	List<Category> list();
+
+}
